a81cf53600d36fd29c203706d3d2943425b9ac32
vairaj <43154018+vairaj@users.noreply.github.com>
2018-10-01 14:38:42 -0600

    Add files via upload
    working on it

---
+++ ListProcessor.cpp
@@ -0,0 +1,54 @@
+// ListProcessor.cpp
+
+// tom bailey   1540  30 sep 2011
+
+// tom bailey   1030  27 jan 2012
+// Exercise the List class with verbose Node constructor
+//   and destructor.
+
+#include "LinkedList.h"
+
+
+int main()
+{
+	List cows;
+	cout << "cows list  :  " << cows << endl << endl;
+
+	cows.insertAsFirst(1.23);
+	cows.insertAsFirst(2.34);
+	cout << "cows list  :  " << cows << endl;
+	cout << "size of cows : " << cows.size() << endl << endl;
+
+	cows.removeFirst();
+	cout << "cows list  :  " << cows << endl << endl;
+
+	List horses(cows);
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl << endl;
+
+	horses.removeFirst();
+	horses.insertAsFirst(5.67);
+	cows.insertAsFirst(6.78);
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl << endl;
+
+	List pigs;
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl;
+	cout << "pigs list  :  " << pigs << endl << endl;
+
+	pigs = cows;
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl;
+	cout << "pigs list  :  " << pigs << endl << endl;
+
+	pigs = horses;
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl;
+	cout << "pigs list  :  " << pigs << endl << endl;
+
+	cout << "End of code" << endl;
+
+	return 0;
+}
+
